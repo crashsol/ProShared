8f87f40f09b3bd2985df9a9521b3db8eec297f36
唐林 <47147551@qq.com>
2018-05-01 09:12:21 +0800

    添加RecommendApi

---
+++ Proshare.RecommendApi/IntergrationEventHandlers/ProjectCreatedIntergrationEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetCore.CAP;
+
+namespace Proshare.RecommendApi.IntergrationEventHandlers
+{
+    public class ProjectCreatedIntergrationEventHandler:ICapSubscribe
+    {
+
+    }
+}
---
+++ Proshare.RecommendApi/IntergrationEvents/ProjectViewedIntergrationEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace Proshare.RecommendApi.IntergrationEvents
+{
+    public class ProjectViewedIntergrationEvent
+    {
+        public string Company { get; set; }
+
+        public string Introduction { get; set; }
+
+       // public ProjectViewer Viewer { get; set; }
+    }
+}
