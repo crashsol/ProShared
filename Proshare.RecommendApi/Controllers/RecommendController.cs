a307adb468e39d720c076ad683acf8ad67caffa0
唐林 <47147551@qq.com>
2018-05-01 22:13:36 +0800

    添加RecommendApi 服务调用方法，并加入Ocelot

---
+++ Proshare.RecommendApi/Controllers/RecommendController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Proshare.RecommendApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProShare.RecommendApi.Controllers
+{
+    [Route("api/Recommends")]
+    public class RecommendController : BaseController
+    {
+        private readonly RecommendDbContext _recommendDbContext;
+    
+        public RecommendController(RecommendDbContext recommendDbContext ,ILogger<RecommendController> logger):base(logger)
+        {
+            _recommendDbContext = recommendDbContext;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetAsync()
+        {
+           return Ok(await _recommendDbContext.ProjectRecommends.AsNoTracking()
+                    .Where(b => b.UserId == UserIdentity.UserId).ToListAsync());
+        }
+
+    }
+}
